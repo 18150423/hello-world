2ab4c56d50714296e4d2c5dd7414f3d79be8b336
wangjie-jie <50397957+wangjie-jie@users.noreply.github.com>
2020-07-21 20:08:47 +0800

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,15 @@
+function rename() {
+	document.getElementById("example").innerHTML = "success";
+}
+function change() {
+	document.getElementById("example").style.fontSize="40px";
+}
+function hide() {
+	document.getElementById("example").style.display="none";
+}
+/*
+function alert{
+	window.alert(66);
+
+}
+	
